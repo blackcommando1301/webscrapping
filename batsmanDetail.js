1e3a8e97ce62a731c73e4ae01819b74de10c4e9e
blackcommando1301 <100750703+blackcommando1301@users.noreply.github.com>
2022-03-30 13:58:18 +0530

    complete

---
+++ batsmanDetail.js
@@ -0,0 +1,39 @@
+const request=require("request");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+const link="https://www.espncricinfo.com/series/indian-premier-league-2022-1298423/chennai-super-kings-vs-kolkata-knight-riders-1st-match-1304047/full-scorecard";
+
+request(link,cb);
+function cb(error,response,html){
+    if(error){
+        console.log(error);
+
+    }else{
+        const dom = new JSDOM(html);
+      const document=dom.window.document;
+      let tabel=document.querySelectorAll(".batsman-cell.text-truncate.out a");
+      for(let i=0;i<tabel.length;i++){
+        let btlink=tabel[i].href;
+        let flink="https://www.espncricinfo.com"+btlink;
+        request(flink,cb2);
+            
+                }
+        
+      }
+     
+
+    }
+    function cb2(error,response,html){
+        if(error){
+            console.log(error);
+        }else{
+            const dom = new JSDOM(html);
+      const document=dom.window.document;
+      let batsmanname=document.querySelectorAll(".player-card-padding .player_overview-grid div h5");
+      console.log( "batsman name-------"+ batsmanname[0].textContent);
+      console.log("batsman DOB---------"+ batsmanname[1].textContent);
+
+
+        }
+    }
--- highestWicketTaker.js
+++ highestWicketTaker.js
@@ -1,7 +1,7 @@
 const request=require("request");
 const jsdom=require("jsdom");
 const {  JSDOM  }=  jsdom;
-const link="https://www.espncricinfo.com/series/ipl-2021-1249214/royal-challengers-bangalore-vs-kolkata-knight-riders-eliminator-1254115/full-scorecard"
+const link="https://www.espncricinfo.com/series/indian-premier-league-2022-1298423/sunrisers-hyderabad-vs-rajasthan-royals-5th-match-1304051/full-scorecard";
 
 request(link,cb);
 function cb(error,response,html){

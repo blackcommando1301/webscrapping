2964b77f2f3e2b7efa67a2f1d56898fa3bf05434
blackcommando1301 <100750703+blackcommando1301@users.noreply.github.com>
2022-03-30 12:00:22 +0530

    complete highest wicket taker

--- highestWicketTaker.js
+++ highestWicketTaker.js
@@ -11,8 +11,32 @@ function cb(error,response,html){
         
         const dom = new JSDOM(html);
       const document=dom.window.document;
-      let names =document.querySelectorAll(".text-nowrap a");
+      let tabel=document.querySelectorAll(".table.bowler");
+      let max=0;
+      let k=0;
+      for(let i=0;i<tabel.length;i++){
+       
+          let row=tabel[i].querySelectorAll("tbody tr");
+          for(let j=0;j<row.length;j++){
+           let td=row[j].querySelectorAll("td");
+            if(td.length>1){
+               let name=td[0].textContent;
+               let wicket=td[4].textContent;
+               if(max<wicket){
+                   max=wicket;
+                   k=name;
+               }
+              
+
+               }
+            }
+            
+          }
+          console.log(max);
+          console.log(k)
+
+      }
+      
       
 
     }
-}
